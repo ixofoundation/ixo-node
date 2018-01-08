47e2419f35a6b6efcb1ceb9150485260ea459a1d
Cedric Franz <cedric.franz@gmail.com>
2018-01-08 10:54:23 +0200

    Added jayson

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
-const express = require('express')
-const path = require('path')
+//const express = require('express')
+//const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+//express()
+//  .use(express.static(path.join(__dirname, 'public')))
+//  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+const jayson = require('jayson');
+  
+ var server = jayson.server({
+   ping: function(args, callback) {
+     // do something, do nothing
+     callback();
+   }
+ });
+  
+ server.http().listen(PORT);
+ console.log(`Listening on ${ PORT }`);
