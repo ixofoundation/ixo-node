7c84ddb48feeef13bd65c9c786a0630d66f02cf0
Cedric Franz <cedric.franz@gmail.com>
2018-01-08 17:42:05 +0200

    Tidied up structure

--- example/ping.js
+++ example/ping.js
@@ -1,7 +1,7 @@
 const jayson = require('jayson');
 
-var client = jayson.client.https('https://arcane-stream-64697.herokuapp.com/network');
-//var client = jayson.client.http('http://localhost:5000/network');
+var client = jayson.client.https('https://arcane-stream-64697.herokuapp.com/api/network');
+//var client = jayson.client.http('http://localhost:5000/api/network');
 client.request('ping', [], null, function(err) {
   if(err) throw err;
   console.log('ok'); // request was received successfully
