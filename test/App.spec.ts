8ad73a130c81ca5e9b80cf3e53cfec3d63470421
Nicolaas Vercuiel <nicolaas.vercuiel73@gmail.com>
2018-01-16 11:33:12 +0200

    # Fixed test timeout issue. # Refactored tests. # Added types for chai-as-promised

--- test/App.spec.ts
+++ test/App.spec.ts
@@ -1,20 +1,17 @@
-import * as mocha from 'mocha';
+import 'mocha';
 import * as chai from 'chai';
-
+import {expect} from 'chai';
 import chaiHttp = require('chai-http');
-
 import app from '../src/App';
 
 chai.use(chaiHttp);
-const expect = chai.expect;
 
 describe('App Tests', () => {
-
-  it('should disply API is running', () => {
-    return chai.request(app).get('/')
-    .then(res => {
-      expect(res.text).to.eql('API is running');
+    it('should disply API is running', () => {
+        return chai.request(app).get('/')
+            .then((res: any) => {
+                expect(res.text).to.eql('API is running');
+            });
     });
-  });
 
 });
--- test/handlers/Handler.spec.ts
+++ test/handlers/Handler.spec.ts
@@ -1,28 +1,28 @@
-import * as mocha from 'mocha';
 import * as chai from 'chai';
-const expect = chai.expect;
-import * as chaiAsPromised from 'chai-as-promised';
-chai.use(chaiAsPromised);
-
+import 'mocha';
+import {expect} from 'chai';
 import {PingHandler} from '../../src/handlers/PingHandler';
 import {ProjectHandler} from '../../src/handlers/ProjectHandler';
+import * as chaiAsPromised from 'chai-as-promised';
 
-describe('Handlers', function() {
-  describe('PingHandler', function() {
-    it('should return pong', function() {
-      var result = new PingHandler().process([]);
-      expect(result).to.eql('pong');
-    });
-  });
+chai.use(chaiAsPromised);
 
-  describe('ProjectHandler', function() {
-    it('should return "default" project template and form', function() {
-      var result = new ProjectHandler().getTemplate({name:"default"})
-        .then((res) => {
-          return res.template['@context']
+describe('Handlers', function () {
+    describe('PingHandler', function () {
+        it('should return pong', function () {
+            var result = new PingHandler().process([]);
+            expect(result).to.eql('pong');
         });
-
-      return expect(result).to.eventually.equal('http://ixo.foundation/schema');
     });
-  });
+
+    describe('ProjectHandler', function () {
+        this.timeout(5000);
+        it('should return "default" project template and form', function () {
+            var result = new ProjectHandler().getTemplate({name: "default"})
+                .then((res) => {
+                    return res.template['@context']
+                });
+            return expect(result).to.eventually.equal('http://ixo.foundation/schema');
+        });
+    });
 });
