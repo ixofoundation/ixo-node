4778271a2946857a32d1bb044016bbdd7bacaedf
Cedric Franz <cedric.franz@gmail.com>
2018-01-24 15:24:00 +0200

    Added caching for templates

--- src/templates/TemplateUtils.ts
+++ src/templates/TemplateUtils.ts
@@ -1,19 +1,30 @@
 import {GitUtils} from './GitUtils';
 
+var templateCache = new Map<string, any>();
+
 export class TemplateUtils { 
 
   gitUtils: GitUtils
   repoName: string
+//  templateCache: Map<string, any>
 
   constructor(){
     this.gitUtils = new GitUtils();
     this.repoName = 'ixofoundation';
+//    this.templateCache = new Map<string, string>();
   }
 
   /*
     Returns the Template and the corresponding form for the name supplied
   */
   getTemplate(templateType: string, name: string){
+    var key = this.getCacheKey(templateType, name);
+    if(templateCache.has(key)){
+      return new Promise((resolve: Function, reject: Function) => {
+        resolve(templateCache.get(key));
+      })
+    }
+
     var template = this.constructTemplate(templateType, name);
     var form = this.constructForm(templateType,name);
 
@@ -21,13 +32,19 @@ export class TemplateUtils {
       .then((templateContents: any) => {
         return this.gitUtils.loadFileContents(this.repoName, form)
         .then((formContents: any) =>{
-          return {
+          var res = {
             template: JSON.parse(templateContents),
             form: JSON.parse(formContents)
           }
+          templateCache.set(key, res);
+          return res;
         });
       });
   }
+
+  getCacheKey(templateType: string, name: string): string {
+    return (templateType + "|" + name).toString();
+}
 
   validateData(data: any, templateType: string, templateName: string){
 
